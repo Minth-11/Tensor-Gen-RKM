e5a9c3a30ac829f5a80a4880bab553af0b031521
Zander Op de Beeck <46244251+Minth-11@users.noreply.github.com>
2024-07-19 13:32:45 +0200

    Cross validation split-up - untested

--- trainDual.py
+++ trainDual.py
@@ -5,6 +5,7 @@ from numpy import linalg as la
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 import scipy as sp
+import random
 
 def main():
 
@@ -19,9 +20,73 @@ def main():
     trData = data["train"]
     xData = trData['x']
     
-    uit = trainDualTensorRBF(xData,hyper,bar=True)
+    fold = 5
+    h = multiViewCross(trData,fold,bar=True)
     
-    sp.io.savemat("dSprites.mat",{"OmegaAdd":OmegaAdd,"OmegaMul":OmegaMul,"gamma":gam})
+    # uit = trainDualTensorRBF(xData,hyper,bar=True)
+    
+    # sp.io.savemat("dSprites.mat",{"OmegaAdd":OmegaAdd,"OmegaMul":OmegaMul,"gamma":gam})
+
+def multiViewCross(data,fold,bar=False):
+    """
+    []          - folds
+    {}          - train/val
+    {}          - x/y
+    []          - views
+    np.array    - data
+    """
+    xData = data['x']
+    yData = data['y']
+    V = len(xData)
+    n = len(xData[0])
+    gts = list(range(fold))
+    idxs = []
+    while len(idxs) < n:
+        idxs = idxs + gts
+    hindCut = len(idxs) - n
+    idxs = idxs[0:(len(idxs)-hindCut)]
+    random.shuffle(idxs)
+    flds = []
+    top = range(fold)
+    if bar:
+        top = tqdm(top)
+        top.set_description("Cross val.")
+    for w in top: # folds
+        # data met index w gaat naar validatie
+        xtr = []
+        xva = []
+        ytr = []
+        yva = []
+        for v in range(V): # views
+            axv = xData[v] # all x-data in current view
+            ayv = yData # all y-data (indep. of view!)
+            xtrv = [] # training x-data in current view
+            ytrv = [] # training y-data in current view
+            xvav = [] # validation x-data in current view
+            yvav = [] # validation y-data in current view
+            for i in range(n): 
+                if (idxs[i] != w):
+                    xtrv.append(axv[i]) # add x training
+                    ytrv.append(ayv[i]) # add y training
+                else:
+                    xvav.append(axv[i]) # add x validation
+                    yvav.append(ayv[i]) # add y validation
+            # make np arrays
+            xtrva = np.array(xtrv)
+            xvava = np.array(xvav)
+            ytrva = np.array(ytrv)
+            yvava = np.array(yvav)
+            # add view into data
+            xtr.append(xtrva)
+            xva.append(xvava)
+            ytr.append(ytrva)
+            yva.append(yvava)
+        # pack-up
+        val   = {'x':yva,'y':yva}
+        train = {'x':ytr,'y':ytr}
+        fl = {"train":train,"val":val}
+        flds.append(fl) # add to folds list
+    return flds
     
 def trainDualTensorRBF(xData,hyper,bar=False):
     gam = hyper[0]
